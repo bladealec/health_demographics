1d77ccde251d3fbc528a6f9206a601d7e761ebd7
Alec Blade <alecblade0@gmail.com>
2023-10-18 22:51:21 -0400

    initial push

---
+++ analysis.sql
@@ -0,0 +1,32 @@
+SELECT *
+FROM health_data
+
+-- 1. Find the top 3 countries with the largest increase in life expectancy from 2005 to 2015.
+
+-- 2. Calculate the average 'Income composition of resources' for countries with a life expectancy above 75 in the year 2010.
+
+-- 3. List the countries that had a decrease in 'Adult Mortality' from 2000 to 2015.
+
+-- 4. Determine the year with the highest total 'percentage expenditure' on healthcare in 'Developed' countries.
+
+-- 5. Find the country with the highest 'Total expenditure' on healthcare as a percentage of GDP in 2014.
+
+-- 6. Calculate the correlation coefficient between 'Income composition of resources' and 'Schooling' across all years and countries.
+
+-- 7. Identify the country that had the steepest increase in 'HIV/AIDS' rate from 2000 to 2015.
+
+-- 8. Calculate the average 'BMI' for countries with a population above 100 million in the year 2013.
+
+-- 9. List the countries that had a decrease in 'thinness 1-19 years' from 2005 to 2015.
+
+-- 10. Find the year when the most countries had a 'Polio' vaccination coverage above 90%.
+
+-- 11. Determine the country with the highest 'Alcohol' consumption that also had a 'Schooling' rate above the global average in 2012.
+
+-- 12. Calculate the average 'Total expenditure' on healthcare as a percentage of GDP for 'Developed' countries in 2015.
+
+-- 13. List the countries with the largest difference between 'thinness 1-19 years' and 'thinness 5-9 years' in the year 2014.
+
+-- 14. Find the year when the average 'GDP' was highest for 'Developing' countries.
+
+-- 15. Determine the country with the highest 'Hepatitis B' vaccination coverage in the year 2013.
